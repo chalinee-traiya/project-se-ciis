40109e607df0636612de3d40b71953c0c708ee18
zaxiffan <zigzagzaczax@gmail.com>
2020-11-05 01:27:31 +0700

    5/11/2563 1:27
    reg

--- src/components/register.js
+++ src/components/register.js
@@ -23,78 +23,61 @@ export default class register extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(evt) {
-    // this.setState({ [evt.target.name]: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   }
   handleSubmit(event) {
+    alert("123");
     var user = firebase.auth().currentUser;
-    // console.log(
-    //    this.state.fname +
-    //     this.state.lname +
-    //     this.state.id +
-    //     this.state.email +
-    //     this.state.username +
-    //     this.state.password +
-    //     this.state.cpassword
-    // );
-    // console.log(this.state.select);
-    firebase
-      .database()
-      .ref("/Check/")
-      .once("value")
-      .then((snapshot) => {
-        // console.log(snapshot.val());
-        try {
-          if (this.state.password != this.state.cpassword) {
-            alert("รหัสผ่านไม่ตรงกัน");
-          } else {
-            snapshot.forEach((element) => {
-              if (element.Email == this.state.email) {
-                firebase
-                  .auth()
-                  .createUserWithEmailAndPassword(
-                    this.state.email,
-                    this.state.password
-                  )
-                  .then(async (user) => {
-                    console.log(user.user.uid);
-                    await this.setState({
-                      uid: user.user.uid,
-                    });
-                  })
-                  .then(() => {
-                    console.log(this.state.uid);
-                    firebase
-                      .database()
-                      .ref("User")
-                      .child(this.state.uid)
-                      .set({
-                        fname: this.state.fname,
-                        lname: this.state.lname,
-                        id: this.state.id,
-                        email: this.state.email,
-                        username: this.state.username,
-                        is_admin: "",
-                        paid: {
-                          status: 0,
-                          timestamp: "",
-                          img: "",
-                        },
-                        nation: this.state.nation,
-                        type: "",
-                      });
-                    alert("สมัครสำเร็จ !");
-                  })
-                  .catch((error) => {
-                    // Handle Errors here.
-                    alert(error);
-                  });
-              }
+    try {
+      if (this.state.password != this.state.cpassword) {
+        alert("รหัสผ่านไม่ตรงกัน");
+      } else {
+        alert(this.state.id.length);
+        if (this.state.id.length == 13) {
+          alert(this.state.id.length);
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(
+              this.state.email,
+              this.state.password
+            )
+            .then(async (user) => {
+              console.log(user.user.uid);
+              await this.setState({
+                uid: user.user.uid,
+              });
+            })
+            .then(() => {
+              console.log(this.state.uid);
+              firebase
+                .database()
+                .ref("User")
+                .child(this.state.uid)
+                .set({
+                  fname: this.state.fname,
+                  lname: this.state.lname,
+                  id: this.state.id,
+                  email: this.state.email,
+                  is_admin: "",
+                  paid: {
+                    status: 0,
+                    timestamp: "",
+                    img: "",
+                  },
+                  nation: "Thai",
+                  type: 5,
+                });
+              alert("สมัครสำเร็จ !");
+            })
+            .catch((error) => {
+              // Handle Errors here.
+              alert(error);
             });
-          }
-        } catch (err) {
-          alert(err);
         }
-      });
+      }
+    } catch (err) {
+      alert(err);
+    }
     event.preventDefault();
   }
 
@@ -126,50 +109,7 @@ export default class register extends Component {
         </nav>
         <div className="auth-wrapper">
           <div className="auth-inner">
-            <div>
-              <h1>Register</h1>
-              <div className="under-register">
-                <p>Register on the platform </p>
-              </div>
-            </div>
-
             <div class="form-row">
-              <div class="form-group col-md-6">
-                <div>
-                  <label for="inputEmail4">
-                    Nationality <span>*</span>
-                  </label>
-                </div>
-
-                <div class="form-check form-check-inline">
-                  <input
-                    class="form-check-input"
-                    type="radio"
-                    name="inlineRadioOptions"
-                    id="inlineRadio1"
-                    value="Thai"
-                    onChange={this.handleChange}
-                    name="nation"
-                  />
-                  <label class="form-check-label" for="inlineRadio1">
-                    Thai
-                  </label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input
-                    class="form-check-input"
-                    type="radio"
-                    name="inlineRadioOptions"
-                    id="inlineRadio2"
-                    value="Foreign"
-                    onChange={this.handleChange}
-                    name="nation"
-                  />
-                  <label class="form-check-label" for="inlineRadio2">
-                    Foreign
-                  </label>
-                </div>
-              </div>
               <div class="form-group col-md-6">
                 <label for="id">
                   ID Card / Passport No.<span>*</span>
@@ -198,7 +138,8 @@ export default class register extends Component {
                   name="fname"
                 />
               </div>
-
+            </div>
+            <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="last-name">
                   Last Name <span>*</span>
@@ -213,7 +154,6 @@ export default class register extends Component {
                 />
               </div>
             </div>
-
             <div class="form-row">
               <div class="form-group col-md-6 ">
                 <label for="input-email">
@@ -228,53 +168,11 @@ export default class register extends Component {
                   name="email"
                 />
               </div>
-
-              <div class="form-group col-md-6">
-                <label for="inputState">
-                  Type identity verification <span>*</span>
-                </label>
-                <select
-                  id="inputState"
-                  class="form-controldrop"
-                  onChange={this.handleChange}
-                  name="type"
-                  required
-                >
-                  <option selected>Choose ...</option>
-                  <option value="1">
-                    Author / Regular Full Paper (Thai Only)
-                  </option>
-                  <option value="2">
-                    Author / Virtual Full Paper(Thai & Foreign)
-                  </option>
-                  <option value="3">
-                    Author / Regular Short Paper (Thai Only)
-                  </option>
-                  <option value="4">
-                    Author / Virtual Short Paper (Thai & Foreign)
-                  </option>
-                  <option value="5">Participant (Thai Only)</option>
-                </select>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="inputusername">
-                Create Username <span>*</span>
-              </label>
-              <input
-                type="username"
-                class="form-control"
-                id="inputusername"
-                placeholder="chalinee1234"
-                onChange={this.handleChange}
-                name="username"
-              />
             </div>
 
             <div class="form-group">
               <label for="inputpassword">
-                Create Password <span>*</span>
+                Password <span>*</span>
               </label>
               <input
                 type="password"
