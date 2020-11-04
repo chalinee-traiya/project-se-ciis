24c045ceedea99196565ad83a296cad4a269d1a1
Piyarat Sangkakun <nan1589@hotmail.co.th>
2020-11-05 00:40:22 +0700

    update form payment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Register from './components/register';
 import Status from './components/Status_page';
 import Payment from './components/Payment';
 import Edit from './components/Edit';
-
+import Form from './components/form/form';
 import { ReturnAdmin } from './components/login';
 // import { auth,db } from '../../services/firebase';
 
@@ -71,7 +71,7 @@ export default class App extends React.Component {
 
           {/* {this.isAdmin?  */}
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Form} />
             <Route path="/sign-in" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/status" component={Status} />
---
+++ src/components/form/form.js
@@ -0,0 +1,70 @@
+import { Hidden } from '@material-ui/core';
+import React, { Component } from 'react';
+import Printer, { print } from 'react-pdf-print'
+import './form.css';
+
+import img from './img/background1.jpg'
+
+class Form extends Component{
+  render() {
+      const mystyle={
+        position:'absolute',
+        left: "50%",
+        marginLeft:'-306px',
+        top:'0px',
+        width:'612px',
+        height:'792px',
+        borderStyle:'outset',
+        overflow:'hidden'
+          
+      }
+
+    return (
+        <div style={mystyle}>
+        <div style={{position:'absolute',left:"0px",top:"0px"}}>
+        <img src={img} style={{width:"612px" ,height:"792px"}}/>
+        </div>
+        <div style={{position:'absolute',left:"250.44px",top:"36.83px"}} class="cls_003"><span class="cls_003">สาขาผู้รับฝาก</span></div>
+        <div style={{position:'absolute',left:"453.83px",top:"36.83px"}} class="cls_003"><span class="cls_003">วันที่</span></div>
+        <div style={{position:'absolute',left:"399.84px",top:"63.95px"}} class="cls_003"><span class="cls_003">โปรดระบุรายละเอียด</span><span class="cls_004">ผู้สมัคร</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"68.99px"}} class="cls_003"><span class="cls_003">คณะวิศวกรรมศาสตรศรราชา มหาวิทยาลัยเกษตรศาสตร์</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"81.83px"}} class="cls_003"><span class="cls_003">ชื่อ</span><span class="cls_002">-</span><span class="cls_003">นามสกุล</span><span class="cls_002"> (Name)</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"85.43px"}} class="cls_003"><span class="cls_003">โทรศัพท์</span><span class="cls_005"> 0-3835-4581</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"100.07px"}}class="cls_003"><span class="cls_003">เลขที่บัตรประชาชน</span><span class="cls_002"> (Ref.1)</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"118.43px"}} class="cls_002"><span class="cls_002">Paper ID (Ref.2)</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"121.43px"}} class="cls_003"><span class="cls_003">ธนาคารทหารไทย จำกัด (มหาชน) Comp. <b>Code : 2087</b></span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"136.55px"}} class="cls_003"><span class="cls_003">เบอรโทรศัพท์มือถือ</span><span class="cls_002"> (Ref.3)</span></div>
+        <div style={{position:'absolute',left:"430.80px",top:"180.59px"}} class="cls_003"><span class="cls_003">จำนวนเงิน (บาท)</span></div>
+        <div style={{position:'absolute',left:"96.24px",top:"189.71px"}}  class="cls_003"><span class="cls_003">โปรดชำระเงินผ่านเคาน์เตอร์ธนาคารทหารไทยทุกสาขาทั่วประเทศ</span></div>
+        <div style={{position:'absolute',left:"435.80px",top:"198.83px"}} class="cls_002"><span class="cls_002">Amount (Baht)</span></div>
+        <div style={{position:'absolute',left:"80.08px",top:"217.67px"}} class="cls_003"><span class="cls_003">เงินสด</span></div>
+        <div style={{position:'absolute',left:"80.08px",top:"235.79px"}} class="cls_002"><span class="cls_002">Cash</span></div>
+        <div style={{position:'absolute',left:"120.72px",top:"242.15px"}} class="cls_003"><span class="cls_002"> โปรดเขียนจำนวนเงินเป็นตัวหนังสือ / Please write amount in words</span></div>
+        <div style={{position:'absolute',left:"51.36px",top:"260.87px"}} class="cls_003"><span class="cls_003">หมายเหตุ - ค่าธรรมเนียม 10  บาททั่วประเทศ ( เรียกเก็บค่าธรรมเนียมกับผู้ชำระเงิน )</span></div>
+        <div style={{position:'absolute',left:"444.00px",top:"325.55px"}} class="cls_003"><span class="cls_003">ผู้รับเงิน</span><span class="cls_002"> / Collector</span></div>
+        <div style={{position:'absolute',left:"51.36px",top:"343.31px"}} class="cls_005"><span class="cls_005">(  สำหรับผู้ส่งบทความ  )</span></div>
+        <div style={{position:'absolute',left:"250.44px",top:"396.83px"}} class="cls_003"><span class="cls_003">สาขาผู้รับฝาก</span></div>
+        <div style={{position:'absolute',left:"453.83px",top:"396.83px"}} class="cls_003"><span class="cls_003">วันที่</span></div>
+        <div style={{position:'absolute',left:"399.84px",top:"423.95px"}} class="cls_003"><span class="cls_003">โปรดระบุรายละเอียด</span><span class="cls_004">ผู้สมัคร</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"430.55px"}} class="cls_003"><span class="cls_003">คณะวิศวกรรมศาสตรศรราชา มหาวิทยาลัยเกษตรศาสตร์</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"441.83px"}} class="cls_003"><span class="cls_003">ชื่อ</span><span class="cls_002">-</span><span class="cls_003">นามสกุล</span><span class="cls_002"> (Name)</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"446.99px"}} class="cls_003"><span class="cls_003">โทรศัพท์</span><span class="cls_005"> 0-3835-4581</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"460.07px"}} class="cls_003"><span class="cls_003">เลขที่บัตรประชาชน</span><span class="cls_002"> (Ref.1)</span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"478.43px"}} class="cls_002"><span class="cls_002">Paper ID (Ref.2)</span></div>
+        <div style={{position:'absolute',left:"111.72px",top:"482.99px"}} class="cls_003"><span class="cls_003">ธนาคารทหารไทย จำกัด (มหาชน) Comp. <b>Code : 2087</b></span></div>
+        <div style={{position:'absolute',left:"357.48px",top:"496.67px"}} class="cls_003"><span class="cls_003">เบอร์โทรศัพท์มือถือ</span><span class="cls_002"> (Ref.3)</span></div>
+        <div style={{position:'absolute',left:"430.80px",top:"542.15px"}} class="cls_003"><span class="cls_003">จำนวนเงิน (บาท)</span></div>
+        <div style={{position:'absolute',left:"96.24px",top:"551.27px"}} class="cls_003"><span class="cls_003">โปรดชำระเงินผ่านเคาน์เตอร์ธนาคารทหารไทยทุกสาขาทั่วประเทศ</span></div>
+        <div style={{position:'absolute',left:"435.80px",top:"560.39px"}} class="cls_002"><span class="cls_002">Amount (Baht)</span></div>
+        <div style={{position:'absolute',left:"80.08px",top:"579.23px"}} class="cls_003"><span class="cls_003">เงินสด</span></div>
+        <div style={{position:'absolute',left:"80.08px",top:"597.35px"}} class="cls_002"><span class="cls_002">Cash</span></div>
+        <div style={{position:'absolute',left:"120.72px",top:"603.71px"}} class="cls_003"><span class="cls_002"> โปรดเขียนจำนวนเงินเป็นตัวหนังสือ / Please write amount in words</span></div>
+        <div style={{position:'absolute',left:"51.36px",top:"622.43px"}} class="cls_003"><span class="cls_003">หมายเหตุ - ค่าธรรมเนียม 10  บาททั่วประเทศ ( เรียกเก็บค่าธรรมเนียมกับผู้ชำระเงิน )</span></div>
+        <div style={{position:'absolute',left:"444.00px",top:"687.11px"}} class="cls_003"><span class="cls_003">ผู้รับเงิน</span><span class="cls_002"> / Collector</span></div>
+        <div style={{position:'absolute',left:"51.36px",top:"704.87px"}} class="cls_005"><span class="cls_005">(  สำหรับเจ้าหน้าที่ธนาคาร  )</span></div>
+        </div>
+    )
+  }
+}
+
+export default Form
